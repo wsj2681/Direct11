cfc515696d6537a158adff34f008928907b0388a
wsj2681 <wsj2681@naver.com>
2024-11-18 10:39:57 +0900

    Polygon 생성함수 분리 + 월드행렬 추가

--- DirectX11/DirectX11/Polygon.cpp
+++ DirectX11/DirectX11/Polygon.cpp
@@ -3,11 +3,24 @@
 
 CPolygon::CPolygon(ComPtr<ID3D11Device>& device, UINT vertexCount)
 {
+	worldMatrix = XMMatrixIdentity();
+	viewMatrix = XMMatrixIdentity();
+	projectionMatrix = XMMatrixIdentity();
+
+	CreatePolygonVertex(vertexCount);
+	CreateVertexBuffer(device);
+	CreateIndexBuffer(device);
+	CreateConstantBuffer(device);
+	CreateShader(device);
+}
+
+void CPolygon::CreatePolygonVertex(UINT vertexCount)
+{
 	const float radius = 0.5f;
 	const float angleIncrement = XM_2PI / vertexCount;
 
 	// Create Vertex
-	vector<Vertex> vertices(vertexCount);
+	vertices.resize(vertexCount);
 	for (int i = 0; i < vertexCount; ++i)
 	{
 		float angle = i * angleIncrement;
@@ -15,7 +28,6 @@ CPolygon::CPolygon(ComPtr<ID3D11Device>& device, UINT vertexCount)
 	}
 
 	// Create Index
-	vector<UINT> indices;
 	for (UINT i = 1; i < vertexCount - 1; ++i)
 	{
 		indices.push_back(0);
@@ -27,8 +39,10 @@ CPolygon::CPolygon(ComPtr<ID3D11Device>& device, UINT vertexCount)
 	indices.push_back(vertexCount - 1);
 	indices.push_back(1);
 	indexCount = indices.size();
+}
 
-	// Vertex Buffer
+void CPolygon::CreateVertexBuffer(ComPtr<ID3D11Device>& device)
+{
 	D3D11_BUFFER_DESC bufferdesc = {};
 	bufferdesc.Usage = D3D11_USAGE_DEFAULT;
 	bufferdesc.ByteWidth = static_cast<UINT>(vertices.size() * sizeof(Vertex));
@@ -39,15 +53,34 @@ CPolygon::CPolygon(ComPtr<ID3D11Device>& device, UINT vertexCount)
 	initdata.pSysMem = vertices.data();
 
 	HR(device->CreateBuffer(&bufferdesc, &initdata, vertexBuffer.GetAddressOf()));
+}
 
-	// Index Buffer
+void CPolygon::CreateIndexBuffer(ComPtr<ID3D11Device>& device)
+{
+	D3D11_BUFFER_DESC bufferdesc = {};
+	bufferdesc.Usage = D3D11_USAGE_DEFAULT;
 	bufferdesc.ByteWidth = static_cast<UINT>(indices.size() * sizeof(UINT));;
 	bufferdesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
 
+	D3D11_SUBRESOURCE_DATA initdata = {};
 	initdata.pSysMem = indices.data();
 
 	HR(device->CreateBuffer(&bufferdesc, &initdata, indexBuffer.GetAddressOf()));
+}
 
+void CPolygon::CreateConstantBuffer(ComPtr<ID3D11Device>& device)
+{
+	D3D11_BUFFER_DESC cbDesc = {};
+	cbDesc.ByteWidth = sizeof(ConstantBuffer);
+	cbDesc.Usage = D3D11_USAGE_DEFAULT;
+	cbDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbDesc.CPUAccessFlags = 0;
+
+	HR(device->CreateBuffer(&cbDesc, nullptr, constantBuffer.GetAddressOf()));
+}
+
+void CPolygon::CreateShader(ComPtr<ID3D11Device>& device)
+{
 	// Vertex Shader
 	ComPtr<ID3DBlob> vsBlob;
 	HR(D3DCompileFromFile(L"triangleShader.hlsl", nullptr, nullptr, "VSMain", "vs_5_0", 0, 0, vsBlob.GetAddressOf(), nullptr));
@@ -70,6 +103,29 @@ CPolygon::CPolygon(ComPtr<ID3D11Device>& device, UINT vertexCount)
 
 void CPolygon::Render(ComPtr<ID3D11DeviceContext>& devcon)
 {
+	worldMatrix = XMMatrixIdentity();
+
+	viewMatrix = XMMatrixLookAtLH(
+		XMVectorSet(0.0f, 2.0f, -5.0f, 1.0f),
+		XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f),
+		XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)
+	);
+
+	projectionMatrix = DirectX::XMMatrixPerspectiveFovLH(
+		XM_PIDIV4,
+		ASPECTRATIO,
+		0.1f,
+		100.0f
+	);
+
+	ConstantBuffer cb = {};
+	cb.world = XMMatrixTranspose(worldMatrix);
+	cb.view = XMMatrixTranspose(viewMatrix);
+	cb.projection = XMMatrixTranspose(projectionMatrix);
+
+	devcon->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cb, 0, 0);
+
+	devcon->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
 	devcon->IASetInputLayout(inputLayout.Get());
 	devcon->VSSetShader(vertexShader.Get(), nullptr, 0);
 	devcon->PSSetShader(pixelShader.Get(), nullptr, 0);
--- DirectX11/DirectX11/Polygon.h
+++ DirectX11/DirectX11/Polygon.h
@@ -6,6 +6,14 @@ struct Vertex
 	XMFLOAT4 color;
 };
 
+//shader input
+struct ConstantBuffer
+{
+	XMMATRIX world;
+	XMMATRIX view;
+	XMMATRIX projection;
+};
+
 class CPolygon
 {
 public:
@@ -15,14 +23,27 @@ public:
 
 private:
 
+	void CreatePolygonVertex(UINT vertexCount);
+	void CreateVertexBuffer(ComPtr<ID3D11Device>& device);
+	void CreateIndexBuffer(ComPtr<ID3D11Device>& device);
+	void CreateConstantBuffer(ComPtr<ID3D11Device>& device);
+	void CreateShader(ComPtr<ID3D11Device>& device);
+
+
+	vector<Vertex> vertices;
+	vector<UINT> indices;
 	ComPtr<ID3D11Buffer> vertexBuffer;
 	ComPtr<ID3D11Buffer> indexBuffer;
+	ComPtr<ID3D11Buffer> constantBuffer;
 	ComPtr<ID3D11VertexShader> vertexShader;
 	ComPtr<ID3D11PixelShader> pixelShader;
 	ComPtr<ID3D11InputLayout> inputLayout;
 
 	UINT indexCount = 0;
 
+	XMMATRIX worldMatrix;
+	XMMATRIX viewMatrix;
+	XMMATRIX projectionMatrix;
 public:
 
 	void Render(ComPtr<ID3D11DeviceContext>& devcon);
